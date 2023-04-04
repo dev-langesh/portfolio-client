8f21654b3f43702cbdf760aaee5e6c1d2529a8e9
langesh <langesh@gmail.com>
2023-04-04 02:08:36 -0400

    spelling update

--- src/components/Home/ProfileDescription.tsx
+++ src/components/Home/ProfileDescription.tsx
@@ -6,8 +6,8 @@ export default function ProfileDescription() {
     <p className="pt-3 text-slate-200 md:text-[20px]">
       I&apos;m a FullStack Developer who loves to build inovative projects.
       I&apos;m in this field for more than 2 years. I have successfully
-      completed 30+ orders from clients under diffrent nations with high ratings
-      and I&apos;m a{" "}
+      completed 30+ orders from clients under different nations with high
+      ratings and I&apos;m a{" "}
       <span className="font-bold text-blue-600">Level One Seller </span> on{" "}
       <Link
         target="_blank"
