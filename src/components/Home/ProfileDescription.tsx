46c42124579a711bf610dbbb1e020ba12fe63f6a
langesh <103199669+dev-langesh@users.noreply.github.com>
2023-04-12 10:22:38 +0000

    Profile desc update

--- src/components/Home/ProfileDescription.tsx
+++ src/components/Home/ProfileDescription.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import React from "react";
+import React, { Children } from "react";
 
 export default function ProfileDescription() {
   return (
     <p className="pt-3 text-slate-200 md:text-[20px]">
       I&apos;m a FullStack Developer who loves to build inovative projects.
-      I&apos;m in this field for more than 2 years. I have successfully
-      completed 30+ orders from clients under different nations with high
+      I&apos;m in this field for more than 2 years. I have completed 35+ orders
+      in <Highlight>Fiverr</Highlight> and 7+ contracts from{" "}
+      <Highlight>Upwork</Highlight> in clients under different nations with high
       ratings.
       {/* and I&apos;m a{" "}
       <span className="font-bold text-blue-600">Level One Seller </span> on{" "}
@@ -19,5 +20,13 @@ export default function ProfileDescription() {
       </Link>
       . */}
     </p>
+  );
+}
+
+function Highlight({ children }: { children: string }) {
+  return (
+    <span className="font-bold font-slate text-blue-600 hover:underline">
+      {children}
+    </span>
   );
 }
