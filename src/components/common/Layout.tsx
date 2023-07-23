fe69ab064c464e038f4de64da3a4aad6df7b6dd4
langesh <dev.langesh@gmail.com>
2023-07-23 21:50:12 +0530

    analytics

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import HorizontalNavBar from "./navbar/HorizontalNavBar";
 import VerticalNavBar from "./navbar/VerticalNavBar";
 
-export default function Layout({ children }: { children: React.ReactElement }) {
+export default function Layout({
+  children,
+}: {
+  children: React.ReactElement[];
+}) {
   return (
     <div className=" bg-gray-900 w-screen h-screen overflow-x-hidden overflow-y-auto text-white font-roboto">
       <HorizontalNavBar />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { store } from "@/reduxapp/store";
 import Layout from "@/components/common/Layout";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </Provider>
   );
